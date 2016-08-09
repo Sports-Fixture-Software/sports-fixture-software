7338ec32d636806f8f664ef2687476f343b3a17e
Craig Keogh <cskeogh@adam.com.au>
2016-08-09 21:58:48 +0930

    Create a database in constructor

--- src/app/components/league_list.component.ts
+++ src/app/components/league_list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LeagueService } from '../services/league.service';
-import * as path from 'path';
+import * as sqlite from 'sqlite3';
 
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
@@ -9,5 +9,16 @@ import * as path from 'path';
 })
 
 export class LeagueListComponent  {
-    constructor(private leagueService: LeagueService) { }
+    private db : sqlite.Database
+    private dbFilename : string = 'sanfl_fixture_software.database'; 
+
+    // TODO: error handling on database open/create
+    // TODO: database filename from a configuration file
+    constructor(private leagueService: LeagueService) {
+        this.db = new sqlite.Database(this.dbFilename, (err: Error) => {
+            if (err) {
+                throw `Unable to open database '${this.dbFilename}'.`
+            }
+        })
+     }
 }
