14e198b98d7cee12fa9f758858ba5dabe1df64bc
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-10 22:06:40 +0930

    Create League model based on #7

--- src/app/models/league.ts
+++ src/app/models/league.ts
@@ -2,7 +2,33 @@
  * League
  */
 export class League {
-    constructor() {
-        
+    private _name : string
+    private _createdOn : Date
+    private _createdBy : string
+    constructor(private theName : string) {
+        name = theName
+    }
+    get name() : string {
+        return this._name
+    }
+    set name(theName : string)
+    {
+        this._name = theName
+    }
+
+    get createdOn() : Date {
+        return this._createdOn
+    }
+    set createdOn(theCreatedOn : Date)
+    {
+        this._createdOn = theCreatedOn
+    }
+
+    get createdBy() :string {
+        return this._createdBy
+    }
+    set createdBy(theCreatedBy : string)
+    {
+        this._createdBy = theCreatedBy
     }
 }
