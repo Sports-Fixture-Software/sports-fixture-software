c9731fced20f203699a72b899b1ee9d8e997785f
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-10 12:51:44 +0930

    Wrong constructor
    This reverts commit 7338ec32d636806f8f664ef2687476f343b3a17e.

--- src/app/components/league_list.component.ts
+++ src/app/components/league_list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LeagueService } from '../services/league.service';
-import * as sqlite from 'sqlite3';
+import * as path from 'path';
 
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
@@ -9,16 +9,5 @@ import * as sqlite from 'sqlite3';
 })
 
 export class LeagueListComponent  {
-    private db : sqlite.Database
-    private dbFilename : string = 'sanfl_fixture_software.database'; 
-
-    // TODO: error handling on database open/create
-    // TODO: database filename from a configuration file
-    constructor(private leagueService: LeagueService) {
-        this.db = new sqlite.Database(this.dbFilename, (err: Error) => {
-            if (err) {
-                throw `Unable to open database '${this.dbFilename}'.`
-            }
-        })
-     }
+    constructor(private leagueService: LeagueService) { }
 }
