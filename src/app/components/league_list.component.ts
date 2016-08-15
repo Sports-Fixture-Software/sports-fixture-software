d8f2daf6d37f68142f7a25b3881bf24515efca3e
Craig Keogh <cskeogh@adam.com.au>
2016-08-16 07:27:19 +0930

    Convert league service to use bluebird

--- src/app/components/league_list.component.ts
+++ src/app/components/league_list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { LeagueService } from '../services/league.service';
 import { League } from '../models/league';
 import * as path from 'path';
+import { DatabaseService } from '../services/database.service'
+import { Collection }  from '../services/collection'
+import * as Promise from 'bluebird'
 
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
@@ -11,13 +14,15 @@ import * as path from 'path';
 
 export class LeagueListComponent implements OnInit {
     private _leagueService : LeagueService
-    leagues : any[]
+    leagues: League[]
 
     constructor(private leagueService: LeagueService) {
         this._leagueService = leagueService
     }
 
     ngOnInit() {
-        this._leagueService.getLeagues().then(l => this.leagues = l)
+        return this._leagueService.getLeagues().then((l) => {
+            this.leagues = l.toArray()
+        })
     }
 }
--- src/app/services/league.service.ts
+++ src/app/services/league.service.ts
@@ -1,61 +1,23 @@
-import { Injectable } from '@angular/core';
 import { League } from '../models/league';
 import * as sqlite from 'sqlite3'
+import { databaseInjector } from '../bootstrap'
+import { DatabaseService } from './database.service'
+import { Collection } from './collection'
+import { Injectable } from '@angular/core'
+import * as Promise from 'bluebird'
 
 @Injectable()
 export class LeagueService {
 
-    private db : sqlite.Database
-    private dbFilename : string = 'sanfl_fixture_software.database'
+    private _db : DatabaseService
 
-    // TODO: error handling on database open/create
-    // TODO: database filename from a configuration file
     constructor() {
-        this.db = new sqlite.Database(this.dbFilename, (err: Error) => {
-            if (err) {
-                throw `Unable to open database '${this.dbFilename}'.`
-            }
-            this.db.serialize(() => {
-                this.db.run(`CREATE TABLE IF NOT EXISTS League
-                (id INTEGER PRIMARY KEY ASC,
-                 name TEXT NOT NULL,
-                 createdOn TEXT,
-                 createdBy TEXT);`)
-            })
-        })
+        this._db = databaseInjector.get(DatabaseService);
      }
 
-    // TODO: error handling
-    public getLeagues() : Promise<League[]> {
-        var promise : Promise<League[]>
-        promise = new Promise<League[]>((resolve, reject) => {
-            this.db.all(`SELECT * FROM League;`, (err: Error, rows: any[]) => {
-                if (err) {
-                    return reject()
-                }
-                var leagues : League[] = []
-                for (var row of rows)
-                {
-                    var createdOn : Date;
-                    if (row.createdOn == null || row.createdOn == undefined)
-                    {
-                        createdOn = null
-                    } else {
-                        createdOn = new Date(row.createdOn)
-                    }
-                    leagues.push(new League(row.id, row.name, createdOn,
-                                            row.createdBy))
-                }
-                resolve(leagues)
-            })
-        })
-        return promise
-    }
-
-    public close()
-    {
-        if (this.db) {
-            this.db.close()
-        }
+    public getLeagues(): Promise<Collection<League>> {
+        //return this._db.init().then(() => {
+            return new League().fetchAll()
+        //})
     }
 }
