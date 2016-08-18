ce88965bad0b6c17dc367b5bf2670d57e902b4d8
Craig Keogh <cskeogh@adam.com.au>
2016-08-18 22:10:45 +0930

    Enable the league presenter as an app provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { LeaguePresenter } from './presenters/league.presenter'
 
 @Component({
     selector: 'app',
-    template : `<router-outlet></router-outlet>` ,
+    template: `<router-outlet></router-outlet>`,
+    providers: [LeaguePresenter],
     directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent{
