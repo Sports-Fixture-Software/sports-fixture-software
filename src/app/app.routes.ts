f37228b3557a0ae85896947300b2a31b7900fa7d
Craig Keogh <cskeogh@adam.com.au>
2016-08-18 22:13:04 +0930

    Add route to new fixture component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 // Import Pages
 import { LeagueListComponent } from './components/league_list.component';
+import { FixtureListComponent } from './components/fixture_list.component';
 
 export const routes: RouterConfig = [
-  { path: '', component: LeagueListComponent }
+  { path: '', component: LeagueListComponent },
+  { path: 'fixture', component: FixtureListComponent },
 ];
 
 export const APP_ROUTER_PROVIDERS = [
