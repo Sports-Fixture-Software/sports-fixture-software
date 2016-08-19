e20ebebb777269de6590c6392f29e2d1c98ba5a2
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-19 22:02:48 +0930

    Add new fixture form

--- src/app/components/fixture_list.component.ts
+++ src/app/components/fixture_list.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
-import { League } from '../models/league';
-import { LeaguePresenter } from '../presenters/league.presenter';
+import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core'
+import { Validators } from '@angular/common'
+import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms'
+import { League } from '../models/league'
+import { Fixture } from '../models/fixture'
+import { LeaguePresenter } from '../presenters/league.presenter'
 
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
-    templateUrl : 'fixture_list.template.html'
+    directives: [REACTIVE_FORM_DIRECTIVES],
+    templateUrl: 'fixture_list.template.html'
 })
 
 export class FixtureListComponent implements OnInit {
     constructor(private _leaguePresenter: LeaguePresenter) {
     }
-
-    activeLeage: League;
+    activeLeage: League
+    fixtureForm: FormGroup
 
     ngOnInit() {
         this.activeLeage = this._leaguePresenter.activeLeague
+        this.fixtureForm = new FormGroup({
+            name: new FormControl('', [<any>Validators.required])
+        })
+    }
+
+    createFixture(fixture: Fixture) {
+        console.log(JSON.stringify(fixture))
     }
 }
