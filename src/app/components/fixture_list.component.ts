d50dafa247db34d78e5766050c49aa8239592203
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-19 22:34:32 +0930

    Save the fixture in the database

--- src/app/components/fixture_list.component.ts
+++ src/app/components/fixture_list.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core'
 import { Validators } from '@angular/common'
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms'
+import { FixtureService } from '../services/fixture.service'
 import { League } from '../models/league'
 import { Fixture } from '../models/fixture'
+import { FixtureForm } from '../models/fixture.form'
 import { LeaguePresenter } from '../presenters/league.presenter'
 
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
     directives: [REACTIVE_FORM_DIRECTIVES],
+    providers: [FixtureService],
     templateUrl: 'fixture_list.template.html'
 })
 
 export class FixtureListComponent implements OnInit {
-    constructor(private _leaguePresenter: LeaguePresenter) {
+    constructor(private _fixtureService: FixtureService,
+        private _leaguePresenter: LeaguePresenter) {
     }
     activeLeage: League
     fixtureForm: FormGroup
@@ -24,7 +28,10 @@ export class FixtureListComponent implements OnInit {
         })
     }
 
-    createFixture(fixture: Fixture) {
-        console.log(JSON.stringify(fixture))
+    createFixture(form: FixtureForm) {
+        let fixture: Fixture = new Fixture()
+        fixture.name = form.name
+        fixture.setLeague(this.activeLeage)
+        this._fixtureService.addFixture(fixture)
     }
 }
