298541a957a1c6b5fe1588ea8261e9dddf9ab86b
Michael Humphris <a1631155@student.adelaide.edu.au>
2016-08-22 11:34:42 +0930

    Created league details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 // Import Pages
 import { LeagueListComponent } from './components/league_list.component';
+import { LeagueComponent } from './components/league.component';
 
 export const routes: RouterConfig = [
-  { path: '', component: LeagueListComponent }
+  { path: '', component: LeagueListComponent },
+  { path: 'league', component: LeagueListComponent },
+  { path: 'league/:id', component: LeagueComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
