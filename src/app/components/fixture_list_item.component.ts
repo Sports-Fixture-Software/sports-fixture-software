d0877ee808a0115bbb31caf07add7a18c6ab1b3f
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-23 20:17:51 +0930

    Align fixutre_list with league_list from #11

--- src/app/components/fixture_list_item.component.ts
+++ src/app/components/fixture_list_item.component.ts
@@ -1,22 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core'
-import { Router } from '@angular/router'
 import { Fixture } from '../models/fixture'
 
 @Component({
-    selector: '[fixture-list-item]',
+    selector: 'fixture-list-item',
     moduleId: module.id.replace(/\\/g, '/'),
     templateUrl: 'fixture_list_item.template.html'
 })
 export class FixtureListItem implements OnInit {
-    @Input('fixture-list-item') fixture: Fixture;
+    @Input() fixture: Fixture;
 
     constructor() {
     }
 
     ngOnInit() {
     }
-
-    selectFixture(fixture: Fixture) {
-
-    }
+    
 }
