ce5c093c7b7d93a48b098d83025dd9cbd9ca9590
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-23 19:58:26 +0930

    Revert "Move the league click code into league_list_item"
    This reverts commit 35244468b307f27d975ab7873ecc2972caf0f7c6.

--- src/app/components/league_list_item.component.ts
+++ src/app/components/league_list_item.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { Router } from '@angular/router'
-import { League } from '../models/league'
+import { Component, OnInit, Input } from '@angular/core';
+import { League } from '../models/league';
 
 @Component({
-    selector: '[league-list-item]',
+    selector: 'league-list-item',
     moduleId: module.id.replace(/\\/g, '/'),
     templateUrl: 'league_list_item.template.html'
 })
 export class LeagueListItem implements OnInit {
-    @Input('league-list-item') league: League;
+    @Input() league: League;
     
-    constructor(private _router: Router) {
+    constructor() {
     }
 
     ngOnInit() {        
     }
 
-    selectLeague(league: League) {
-        this._router.navigate(['league', league.id])
-    }
 }
