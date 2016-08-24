3a553b965aee96d9dabb1f310c6bea6632df5a9d
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-24 21:29:21 +0930

    Add team_list component for teams page

---
+++ src/app/components/team_list.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core'
+import { Router, ActivatedRoute } from '@angular/router'
+import { Team } from '../models/team'
+import { League } from '../models/league'
+import { LeagueService } from '../services/league.service'
+import { TeamService } from '../services/team.service'
+import { Collection }  from '../services/collection'
+import * as Promise from 'bluebird'
+import { Navbar } from './navbar.component';
+import { TeamListItem } from './team_list_item.component';
+import { POPOVER_DIRECTIVES } from 'ng2-popover';
+import { MODAL_DIRECTIVES, ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+
+@Component({
+    moduleId: module.id.replace(/\\/g, '/'),
+    templateUrl: 'team_list.template.html',
+    providers: [LeagueService, TeamService],
+    directives: [TeamListItem, POPOVER_DIRECTIVES, MODAL_DIRECTIVES]
+})
+
+export class TeamListComponent implements OnInit {
+    /**
+     * ## API
+     * - `changeref` (provided by the injector)
+     *    Angular2's change detector auto-detects Events, XHR, & Timers. For
+     *    `bookshelf` data, we have call the change detector when data changes
+     */
+    constructor(private _leagueService: LeagueService,
+        private _teamService: TeamService,
+        private _changeref: ChangeDetectorRef,
+        private _router: Router,
+        private _route: ActivatedRoute) {
+    }
+    newTeamText: String
+
+    get teams(): Team[] { return this._teams }
+    set teams(value: Team[]) { this._teams = value }
+    get league(): League { return this._league }
+    set league(value: League) { this._league = value }
+
+    ngOnInit() {
+        this._router.routerState.parent(this._route)
+            .params.forEach(params => {
+                let id = +params['id'];
+                this._leagueService.getLeague(id).then((l) => {
+                    this.league = l
+                    return l.getTeams()
+                }).then((t) => {
+                    this.teams = t.toArray()
+                    this._changeref.detectChanges()
+                })
+            })
+    }
+
+    submitAddTeam(teamName: string) {
+        let team: Team = new Team(teamName)
+        team.setLeague(this.league)
+        this._teamService.addTeam(team).then((t) => {
+            this.teams.push(team)
+            this._changeref.detectChanges()
+        })
+    }
+
+    private _teams: Team[]
+    private _league: League
+}
