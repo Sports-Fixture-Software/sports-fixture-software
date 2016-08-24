0357caa37b3271504be2d4175a7de66cd84a2c6f
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-24 21:28:36 +0930

    Add team service

---
+++ src/app/services/team.service.ts
@@ -0,0 +1,32 @@
+import { League } from '../models/league';
+import { Team } from '../models/team';
+import { Collection } from './collection'
+import { Injectable } from '@angular/core'
+import * as Promise from 'bluebird'
+
+@Injectable()
+export class TeamService {
+
+    getTeams(leagueId?: number): Promise<Collection<Team>> {
+        if (leagueId) {
+            return new Team().where('league_id', leagueId).fetchAll()
+        } else {
+            return new Team().fetchAll()
+        }
+    }
+
+    getTeam(id: number): Promise<Team> {
+        return new Team().where('id', id).fetch()
+    }
+
+    public addTeam(team: Team): Promise<Team> {
+        return team.save()
+    }
+
+    /**
+     * returns an empty Team
+     */
+    deleteTeam(team: Team): Promise<Team> {
+        return team.destroy()
+    }
+}
