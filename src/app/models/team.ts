b175022ac7090f1cb92348a29d62b86b3f35258f
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-24 16:25:08 +0930

    Add team model

---
+++ src/app/models/team.ts
@@ -0,0 +1,35 @@
+import { databaseInjector } from '../bootstrap'
+import { DatabaseService } from '../services/database.service'
+import { League } from './league'
+import * as Promise from 'bluebird'
+
+export class Team extends (databaseInjector.get(DatabaseService) as DatabaseService).Model<Team> {
+
+    constructor(params?: string | any) {
+        if (typeof params === 'string') {
+            super()
+            this.name = params
+        } else {
+            super(params)
+        }
+    }
+
+    get tableName() { return 'team'; }
+
+    get name(): string { return this.get('name') }
+    set name(value: string) { this.set('name', value) }
+
+    getLeague(): Promise<League> {
+        return this.fetch({ withRelated: ['league'] }).then((res) => {
+            return res.related('league') as League
+        })
+    }
+    setLeague(value: League) { this.set('league_id', value.id) }
+
+    /**
+     * Needed by bookshelf to setup relationship
+     */
+    protected league() {
+        return this.belongsTo(League)
+    }
+}
