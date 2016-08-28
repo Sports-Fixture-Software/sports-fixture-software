ab437ca87e9a1e735a5a8c5034371d22d12ba050
Craig S. Keogh <cskeogh@adam.com.au>
2016-08-28 10:43:13 +0930

    Add the fixture details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LeagueListComponent } from './components/league_list.component';
 import { TeamListComponent } from './components/team_list.component';
 import { FixtureComponent } from './components/fixture.component';
 import { FixtureListComponent } from './components/fixture_list.component';
+import { FixtureDetailsComponent } from './components/fixture_details.component';
 import { LeagueComponent } from './components/league.component';
 import { LeagueDetailsComponent } from './components/league_details.component';
 
@@ -23,7 +24,11 @@ export const routes: RouterConfig = [
   },
   {
     path: 'fixture/:id',
-    component: FixtureComponent
+    component: FixtureComponent,
+    children: [
+      { path: '', redirectTo: 'details', pathMatch: 'full' },
+      { path: 'details', component: FixtureDetailsComponent }
+    ]
   }
 ];
 
