a34045e63ebf7aceed37aa8ee9fc6af692e61802
Craig S. Keogh <cskeogh@adam.com.au>
2016-09-09 06:55:00 +0930

    Move search spec into src/app
    I don't like the tests in the source tree, and plan to move to a test
    tree, but testing the framework for now

---
+++ src/app/util/search.spec.ts
@@ -0,0 +1,13 @@
+import { Search } from './search'
+
+export function main() {
+    describe('binary search', () => {
+        it('empty array', () => {
+            let index = Search.binarySearch([], 5, (a: number, b: number) => {
+                return a - b
+            })
+            expect(index).toBe(-1)
+        })
+    })
+}
+class TestSearch { }
