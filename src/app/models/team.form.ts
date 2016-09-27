89ed1e6bdcd7c1be10ee6747cc317ea64b6d770d
Craig S. Keogh <cskeogh@adam.com.au>
2016-09-27 21:47:24 +0930

    Add team level constraints to team form
    Team level constraints as agreed in Meeting 10:
    https://github.com/serp2016/sanfl/wiki/Meetings%5CMinutes10

--- src/app/models/team.form.ts
+++ src/app/models/team.form.ts
@@ -2,5 +2,11 @@ import { Team } from './team'
 
 export interface TeamForm {
     name: string,
+    homeGamesMin: number,
+    homeGamesMax: number,
+    homeGamesEnabled: boolean,
+    awayGamesMin: number,
+    awayGamesMax: number,
+    awayGamesEnabled: boolean,
     team: Team
 }
