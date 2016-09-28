75233aad04c9ac6c4ce86d57fb18a81b68f54099
Craig S. Keogh <cskeogh@adam.com.au>
2016-09-28 21:53:17 +0930

    Add FileFolder's createWriteStream
    createWriteStream creates a buffered write stream at the location
    specified by `filename`.

---
+++ src/app/util/file_folder.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs'
+import * as Promise from 'bluebird'
+
+export class FileFolder {
+
+    /**
+     * Creates a buffered write stream at the location specified by `filename`.
+     * If the creation was unsuccessful, an error is thrown.
+     */
+    static createWriteStream(filename: string): Promise<fs.WriteStream> {
+        return new Promise<fs.WriteStream>((resolve, reject) => {
+            if (filename && filename.length > 0) {
+                let stream = fs.createWriteStream(filename)
+                stream.on('open', () => {
+                    stream.cork()
+                    return resolve(stream)
+                })
+                stream.on('error', (err: Error) => {
+                    return reject(err)
+                })
+            } else {
+                return reject(new Error(`Unable to save to the location '${filename}'`))
+            }
+        })
+    }
+}
