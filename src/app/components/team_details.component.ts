8605c7264b3dae417fa857e14a02dfd9eebe1d49
Craig S. Keogh <cskeogh@adam.com.au>
2016-09-29 22:07:46 +0930

    Populate team form with database data
    Populate the team form from the database, and save changes back to the
    database. Enabled checkboxes still to do.

--- src/app/components/team_details.component.ts
+++ src/app/components/team_details.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router'
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms'
 import { Team } from '../models/team'
 import { TeamService } from '../services/team.service'
+import { TeamConfigService } from '../services/team_config.service'
 import { ButtonPopover } from './button_popover.component'
 import { TeamForm } from '../models/team.form'
 import { Subscription } from 'rxjs/Subscription'
@@ -11,7 +12,7 @@ import { Subscription } from 'rxjs/Subscription'
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
     templateUrl: 'team_details.template.html',
-    providers: [TeamService],
+    providers: [TeamService, TeamConfigService],
     directives: [ButtonPopover, REACTIVE_FORM_DIRECTIVES]
 })
 
@@ -19,6 +20,7 @@ export class TeamDetailsComponent implements OnInit {
     constructor(private route: ActivatedRoute,
         private router: Router,
         private teamService: TeamService,
+        private teamConfigService: TeamConfigService,
         private changeref: ChangeDetectorRef) {
     }
 
@@ -36,10 +38,14 @@ export class TeamDetailsComponent implements OnInit {
             awayGamesMax: new FormControl('', [<any>Validators.required]),
             awayGamesEnabled: new FormControl('', [<any>Validators.required]),
         })
-        this.router.routerState.parent(this.route)
-            .params.forEach(params => {
-                let id = +params['id']
+        this.route.params.forEach(params => {
+            let id = +params['id']
+            this.teamService.getTeamAndConfig(id).then(team => {
+                this.team = team
+                this.resetForm()
+                this.changeref.detectChanges()
             })
+        })
     }
 
     onEditTeam() {
@@ -55,9 +61,28 @@ export class TeamDetailsComponent implements OnInit {
 
     updateTeam(form: TeamForm) {
         this.team.name = form.name
+        this.teamService.addTeam(this.team).then((f) => {
+            this.editing = false
+            this.changeref.detectChanges()
+        }).catch((err: Error) => {
+            this.saveChangesButton.showError('Error saving changes', err.message)
+            this.changeref.detectChanges()
+        })
     }
 
     private resetForm() {
+        let fc = this.teamForm.controls['name'] as FormControl
+        fc.updateValue(this.team.name)
+        if (this.team.teamConfigPreLoaded) {
+            fc = this.teamForm.controls['homeGamesMin'] as FormControl
+            fc.updateValue(this.team.teamConfigPreLoaded.homeGamesMin)
+            fc = this.teamForm.controls['homeGamesMax'] as FormControl
+            fc.updateValue(this.team.teamConfigPreLoaded.homeGamesMax)
+            fc = this.teamForm.controls['awayGamesMin'] as FormControl
+            fc.updateValue(this.team.teamConfigPreLoaded.awayGamesMin)
+            fc = this.teamForm.controls['awayGamesMax'] as FormControl
+            fc.updateValue(this.team.teamConfigPreLoaded.awayGamesMax)
+        }
     }
 
     private team: Team
