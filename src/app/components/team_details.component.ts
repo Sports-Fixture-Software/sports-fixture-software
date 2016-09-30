a70e964b7f619117c2fa8d8189af17e07038c9ba
Craig S. Keogh <cskeogh@adam.com.au>
2016-09-30 17:52:28 +0930

    Handle enabled checkbox for TeamDetails form
    If the user enters a value, then check the enabled checkbox. If the user
    clears the checkbox, clear the entered values.
    
    Save the TeamConfig to the database.

--- src/app/components/team_details.component.ts
+++ src/app/components/team_details.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core'
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, ViewChild } from '@angular/core'
 import { Validators } from '@angular/common'
 import { ActivatedRoute, Router } from '@angular/router'
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms'
 import { Team } from '../models/team'
+import { TeamConfig } from '../models/team_config'
 import { TeamService } from '../services/team.service'
 import { TeamConfigService } from '../services/team_config.service'
 import { ButtonPopover } from './button_popover.component'
@@ -16,7 +17,7 @@ import { Subscription } from 'rxjs/Subscription'
     directives: [ButtonPopover, REACTIVE_FORM_DIRECTIVES]
 })
 
-export class TeamDetailsComponent implements OnInit {
+export class TeamDetailsComponent implements OnInit, OnDestroy {
     constructor(private route: ActivatedRoute,
         private router: Router,
         private teamService: TeamService,
@@ -48,6 +49,13 @@ export class TeamDetailsComponent implements OnInit {
         })
     }
 
+    ngOnDestroy() {
+        for (let i in this.listeners) {
+            let listener = this.listeners[i] as Subscription
+            listener.unsubscribe()
+        }
+    }
+
     onEditTeam() {
         this.editing = true
         this.changeref.detectChanges()
@@ -59,9 +67,83 @@ export class TeamDetailsComponent implements OnInit {
         this.changeref.detectChanges()
     }
 
+    /**
+     *  If the user unchecks, clear the entered values
+     */
+    onHomeGamesEnabledChange(value: boolean) {
+        if (!value) {
+            let fc = this.teamForm.controls['homeGamesMin'] as FormControl
+            fc.updateValue(null, {emitEvent : false})
+            fc = this.teamForm.controls['homeGamesMax'] as FormControl
+            fc.updateValue(null, {emitEvent : false})
+        }
+    }
+
+    /**
+     *  If the user unchecks, clear the entered values
+     */
+    onAwayGamesEnabledChange(value: boolean) {
+        if (!value) {
+            let fc = this.teamForm.controls['awayGamesMin'] as FormControl
+            fc.updateValue(null, {emitEvent : false})
+            fc = this.teamForm.controls['awayGamesMax'] as FormControl
+            fc.updateValue(null, {emitEvent : false})
+        }
+    }
+
+    /**
+     *  If the user enters a value, check the box to enable. If the user is
+     *  entering a value, it is assumed they want enabled checked
+     */
+    onHomeGamesChange(value: string) {
+        if (value) {
+            let fc = this.teamForm.controls['homeGamesEnabled'] as FormControl
+            fc.updateValue(true, {emitEvent : false})
+        }
+    }
+
+    /**
+     *  If the user enters a value, check the box to enable. If the user is
+     *  entering a value, it is assumed they want enabled checked
+     */
+    onAwayGamesChange(value: string) {
+        if (value) {
+            let fc = this.teamForm.controls['awayGamesEnabled'] as FormControl
+            fc.updateValue(true, {emitEvent : false})
+        }
+    }
+
     updateTeam(form: TeamForm) {
         this.team.name = form.name
-        this.teamService.addTeam(this.team).then((f) => {
+        let config = this.team.teamConfigPreLoaded
+        if (!config) {
+            config = new TeamConfig()
+            config.setTeam(this.team)
+            this.team.teamConfigPreLoaded = config
+        }
+        config.homeGamesMin = Number.parseInt(form.homeGamesMin)
+        config.homeGamesMin = Number.isInteger(config.homeGamesMin) ? config.homeGamesMin : null
+        config.homeGamesMax = Number.parseInt(form.homeGamesMax)
+        config.homeGamesMax = Number.isInteger(config.homeGamesMax) ? config.homeGamesMax : null
+        config.awayGamesMin = Number.parseInt(form.awayGamesMin)
+        config.awayGamesMin = Number.isInteger(config.awayGamesMin) ? config.awayGamesMin : null
+        config.awayGamesMax = Number.parseInt(form.awayGamesMax)
+        config.awayGamesMax = Number.isInteger(config.awayGamesMax) ? config.awayGamesMax : null
+        // if user checked the checkbox, but didn't enter a value, turn the
+        // checked off
+        if (form.homeGamesEnabled && (form.homeGamesMin == null || form.homeGamesMin == '') && (form.homeGamesMax == null || form.homeGamesMax == '')) {
+            let fc = this.teamForm.controls['homeGamesEnabled'] as FormControl
+            fc.updateValue(false, {emitEvent : false})
+        }
+        // if user checked the checkbox, but didn't enter a value, turn the
+        // checked off
+        if (form.awayGamesEnabled && (form.awayGamesMin == null || form.awayGamesMin == '') && (form.awayGamesMax == null || form.awayGamesMax == '')) {
+            let fc = this.teamForm.controls['awayGamesEnabled'] as FormControl
+            fc.updateValue(false, {emitEvent : false})
+        }
+        this.teamService.addTeam(this.team).then(() => {
+            return this.teamConfigService.addTeamConfig(config)
+        }).then(() => {
             this.editing = false
             this.changeref.detectChanges()
         }).catch((err: Error) => {
@@ -76,14 +158,58 @@ export class TeamDetailsComponent implements OnInit {
         if (this.team.teamConfigPreLoaded) {
             fc = this.teamForm.controls['homeGamesMin'] as FormControl
             fc.updateValue(this.team.teamConfigPreLoaded.homeGamesMin)
+            if (!this.listeners.homeGamesMin) {
+                this.listeners.homeGamesMin = fc.valueChanges.subscribe((evt) => {
+                    this.onHomeGamesChange(evt)
+                })
+            }
             fc = this.teamForm.controls['homeGamesMax'] as FormControl
             fc.updateValue(this.team.teamConfigPreLoaded.homeGamesMax)
+            if (!this.listeners.homeGamesMax) {
+                this.listeners.homeGamesMax = fc.valueChanges.subscribe((evt) => {
+                    this.onHomeGamesChange(evt)
+                })
+            }
             fc = this.teamForm.controls['awayGamesMin'] as FormControl
             fc.updateValue(this.team.teamConfigPreLoaded.awayGamesMin)
+            if (!this.listeners.awayGamesMin) {
+                this.listeners.awayGamesMin = fc.valueChanges.subscribe((evt) => {
+                    this.onAwayGamesChange(evt)
+                })
+            }
             fc = this.teamForm.controls['awayGamesMax'] as FormControl
             fc.updateValue(this.team.teamConfigPreLoaded.awayGamesMax)
+            if (!this.listeners.awayGamesMax) {
+                this.listeners.awayGamesMax = fc.valueChanges.subscribe((evt) => {
+                    this.onAwayGamesChange(evt)
+                })
+            }
+        }
+        fc = this.teamForm.controls['homeGamesEnabled'] as FormControl
+        fc.updateValue(this.team.teamConfigPreLoaded && (this.team.teamConfigPreLoaded.homeGamesMin != null || this.team.teamConfigPreLoaded.homeGamesMax != null))
+        if (!this.listeners.homeGamesEnabled) {
+            this.listeners.homeGamesEnabled = fc.valueChanges.subscribe((evt) => {
+                this.onHomeGamesEnabledChange(evt)
+            })
+        }
+        fc = this.teamForm.controls['awayGamesEnabled'] as FormControl
+        fc.updateValue(this.team.teamConfigPreLoaded && (this.team.teamConfigPreLoaded.awayGamesMin != null || this.team.teamConfigPreLoaded.awayGamesMax != null))
+        if (!this.listeners.awayGamesEnabled) {
+            this.listeners.awayGamesEnabled = fc.valueChanges.subscribe((evt) => {
+                this.onAwayGamesEnabledChange(evt)
+            })
         }
     }
 
     private team: Team
+    private listeners : listenerType = {} as listenerType
+}
+
+interface listenerType {
+        homeGamesMin: Subscription,
+        homeGamesMax: Subscription,
+        homeGamesEnabled: Subscription,
+        awayGamesMin: Subscription,
+        awayGamesMax: Subscription,
+        awayGamesEnabled: Subscription
 }
