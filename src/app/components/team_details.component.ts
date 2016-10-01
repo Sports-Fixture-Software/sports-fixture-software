1962c36eff497a5d689cf175735f391bddfa9010
Craig S. Keogh <cskeogh@adam.com.au>
2016-10-01 09:51:39 +0930

    Add field validation for team's integer fields
    Check the team constraints fields: ensure the user entered value is blank
    or a whole non-negative number. A popover is displayed on inform the user
    if an error.
    
    Only HomeGamesMin is implemented.

--- src/app/components/team_details.component.ts
+++ src/app/components/team_details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef, ViewChild } from '@angular/core'
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core'
 import { Validators } from '@angular/common'
 import { ActivatedRoute, Router } from '@angular/router'
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms'
@@ -9,6 +9,8 @@ import { TeamConfigService } from '../services/team_config.service'
 import { ButtonPopover } from './button_popover.component'
 import { TeamForm } from '../models/team.form'
 import { Subscription } from 'rxjs/Subscription'
+import * as twitterBootstrap from 'bootstrap'
+declare var jQuery: JQueryStatic
 
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
@@ -26,19 +28,27 @@ export class TeamDetailsComponent implements OnInit, OnDestroy {
     }
 
     @ViewChild('saveChangesButton') saveChangesButton: ButtonPopover
+    @ViewChild('homeGamesMinInput') homeGamesMinInput: ElementRef
+    @ViewChild('homeGamesMaxInput') homeGamesMaxInput: ElementRef
+    @ViewChild('awayGamesMinInput') awayGamesMinInput: ElementRef
+    @ViewChild('awayGamesMaxInput') awayGamesMaxInput: ElementRef
     editing: boolean = false
     teamForm: FormGroup
 
     ngOnInit() {
         this.teamForm = new FormGroup({
             name: new FormControl('', [<any>Validators.required]),
-            homeGamesMin: new FormControl('', [<any>Validators.required]),
-            homeGamesMax: new FormControl('', [<any>Validators.required]),
-            homeGamesEnabled: new FormControl('', [<any>Validators.required]),
-            awayGamesMin: new FormControl('', [<any>Validators.required]),
-            awayGamesMax: new FormControl('', [<any>Validators.required]),
-            awayGamesEnabled: new FormControl('', [<any>Validators.required]),
+            homeGamesMin: new FormControl('', [this.numberValidator]),
+            homeGamesMax: new FormControl('', [this.numberValidator]),
+            homeGamesEnabled: new FormControl(),
+            awayGamesMin: new FormControl('', [this.numberValidator]),
+            awayGamesMax: new FormControl('', [this.numberValidator]),
+            awayGamesEnabled: new FormControl(),
         })
+        this.fieldData.homeGamesMin = { state: State.Hidden } as PopoverFieldType
+        this.fieldData.homeGamesMax = { state: State.Hidden } as PopoverFieldType
+        this.fieldData.awayGamesMin = { state: State.Hidden } as PopoverFieldType
+        this.fieldData.awayGamesMax = { state: State.Hidden } as PopoverFieldType
         this.route.params.forEach(params => {
             let id = +params['id']
             this.teamService.getTeamAndConfig(id).then(team => {
@@ -73,9 +83,9 @@ export class TeamDetailsComponent implements OnInit, OnDestroy {
     onHomeGamesEnabledChange(value: boolean) {
         if (!value) {
             let fc = this.teamForm.controls['homeGamesMin'] as FormControl
-            fc.updateValue(null, {emitEvent : false})
+            fc.updateValue(null, { emitEvent: false })
             fc = this.teamForm.controls['homeGamesMax'] as FormControl
-            fc.updateValue(null, {emitEvent : false})
+            fc.updateValue(null, { emitEvent: false })
         }
     }
 
@@ -85,31 +95,52 @@ export class TeamDetailsComponent implements OnInit, OnDestroy {
     onAwayGamesEnabledChange(value: boolean) {
         if (!value) {
             let fc = this.teamForm.controls['awayGamesMin'] as FormControl
-            fc.updateValue(null, {emitEvent : false})
+            fc.updateValue(null, { emitEvent: false })
             fc = this.teamForm.controls['awayGamesMax'] as FormControl
-            fc.updateValue(null, {emitEvent : false})
+            fc.updateValue(null, { emitEvent: false })
         }
     }
 
     /**
+     *  Show an error popover if invalid user entry.
      *  If the user enters a value, check the box to enable. If the user is
-     *  entering a value, it is assumed they want enabled checked
+     *  entering a value, it is assumed they want enabled checked.
      */
-    onHomeGamesChange(value: string) {
-        if (value) {
-            let fc = this.teamForm.controls['homeGamesEnabled'] as FormControl
-            fc.updateValue(true, {emitEvent : false})
+    onFieldChange(value: any, element: ElementRef, control: FormControl, controlPair: FormControl, enableControl: FormControl, popoverData: PopoverFieldType) {
+        // if value blank and the control's pair is blank, disable
+        if (typeof value === 'string' && value.trim() == '' && typeof controlPair.value === 'string' && controlPair.value.trim() == '' && enableControl) {
+            enableControl.updateValue(false, { emitEvent: false })
+        } else if (value && enableControl) {
+            // user entered a value, assume they want enabled
+            enableControl.updateValue(true, { emitEvent: false })
         }
-    }
-
-    /**
-     *  If the user enters a value, check the box to enable. If the user is
-     *  entering a value, it is assumed they want enabled checked
-     */
-    onAwayGamesChange(value: string) {
-        if (value) {
-            let fc = this.teamForm.controls['awayGamesEnabled'] as FormControl
-            fc.updateValue(true, {emitEvent : false})
+        if (control.valid) {
+            if (popoverData.element) {
+                if (popoverData.state == State.Shown) {
+                    popoverData.element.popover('hide')
+                }
+            }
+        }
+        else {
+            if (popoverData.element) {
+                if (popoverData.state == State.Hidden) {
+                    popoverData.element.popover('show')
+                } else if (popoverData.state == State.Hidding) {
+                    popoverData.element.one('hidden.bs.popover', () => {
+                        popoverData.element.popover('show')
+                    })
+                }
+            } else {
+                popoverData.element = jQuery(element.nativeElement).popover({ html: true, template: this.popoverTemplate, content: 'Please enter a number', placement: 'bottom', trigger: 'manual' }).on('show.bs.popover', () => {
+                    popoverData.state = State.Showing
+                }).on('shown.bs.popover', () => {
+                    popoverData.state = State.Shown
+                }).on('hide.bs.popover', () => {
+                    popoverData.state = State.Hidding
+                }).on('hidden.bs.popover', () => {
+                    popoverData.state = State.Hidden
+                }).popover('show')
+            }
         }
     }
 
@@ -132,13 +163,13 @@ export class TeamDetailsComponent implements OnInit, OnDestroy {
         // checked off
         if (form.homeGamesEnabled && (form.homeGamesMin == null || form.homeGamesMin == '') && (form.homeGamesMax == null || form.homeGamesMax == '')) {
             let fc = this.teamForm.controls['homeGamesEnabled'] as FormControl
-            fc.updateValue(false, {emitEvent : false})
+            fc.updateValue(false, { emitEvent: false })
         }
         // if user checked the checkbox, but didn't enter a value, turn the
         // checked off
         if (form.awayGamesEnabled && (form.awayGamesMin == null || form.awayGamesMin == '') && (form.awayGamesMax == null || form.awayGamesMax == '')) {
             let fc = this.teamForm.controls['awayGamesEnabled'] as FormControl
-            fc.updateValue(false, {emitEvent : false})
+            fc.updateValue(false, { emitEvent: false })
         }
         this.teamService.addTeam(this.team).then(() => {
             return this.teamConfigService.addTeamConfig(config)
@@ -159,28 +190,31 @@ export class TeamDetailsComponent implements OnInit, OnDestroy {
             fc.updateValue(this.team.teamConfigPreLoaded.homeGamesMin)
             if (!this.listeners.homeGamesMin) {
                 this.listeners.homeGamesMin = fc.valueChanges.subscribe((evt) => {
-                    this.onHomeGamesChange(evt)
+                    this.onFieldChange(evt, this.homeGamesMinInput, this.teamForm.controls['homeGamesMin'] as FormControl,
+                        this.teamForm.controls['homeGamesMax'] as FormControl, this.teamForm.controls['homeGamesEnabled'] as FormControl, this.fieldData.homeGamesMin)
                 })
             }
             fc = this.teamForm.controls['homeGamesMax'] as FormControl
             fc.updateValue(this.team.teamConfigPreLoaded.homeGamesMax)
             if (!this.listeners.homeGamesMax) {
                 this.listeners.homeGamesMax = fc.valueChanges.subscribe((evt) => {
-                    this.onHomeGamesChange(evt)
+                    this.onFieldChange(evt, this.homeGamesMaxInput, this.teamForm.controls['homeGamesMax'] as FormControl,
+                        this.teamForm.controls['homeGamesMin'] as FormControl, this.teamForm.controls['homeGamesEnabled'] as FormControl, this.fieldData.homeGamesMax)
                 })
             }
             fc = this.teamForm.controls['awayGamesMin'] as FormControl
             fc.updateValue(this.team.teamConfigPreLoaded.awayGamesMin)
             if (!this.listeners.awayGamesMin) {
                 this.listeners.awayGamesMin = fc.valueChanges.subscribe((evt) => {
-                    this.onAwayGamesChange(evt)
+                    this.onFieldChange(evt, this.awayGamesMinInput, this.teamForm.controls['awayGamesMin'] as FormControl, this.teamForm.controls['awayGamesMax'] as FormControl, this.teamForm.controls['awayGamesEnabled'] as FormControl, this.fieldData.awayGamesMin)
                 })
             }
             fc = this.teamForm.controls['awayGamesMax'] as FormControl
             fc.updateValue(this.team.teamConfigPreLoaded.awayGamesMax)
             if (!this.listeners.awayGamesMax) {
                 this.listeners.awayGamesMax = fc.valueChanges.subscribe((evt) => {
-                    this.onAwayGamesChange(evt)
+                    this.onFieldChange(evt, this.awayGamesMaxInput, this.teamForm.controls['awayGamesMax'] as FormControl,
+                        this.teamForm.controls['awayGamesMin'] as FormControl, this.teamForm.controls['awayGamesEnabled'] as FormControl, this.fieldData.awayGamesMax)
                 })
             }
         }
@@ -200,15 +234,49 @@ export class TeamDetailsComponent implements OnInit, OnDestroy {
         }
     }
 
+    /**
+     * Validator to ensure a non-negative whole number number, or blank.
+     */
+    private numberValidator = (control: FormControl): { [key: string]: any } => {
+        if (control.value == null) {
+            return null
+        }
+        if (typeof control.value === 'string' && control.value.trim() == '') {
+            return null
+        }
+        if (Number(control.value) < 0) {
+            return { Negative: true }
+        }
+        return Number.isInteger(Number(control.value)) ? null : { NaN: true }
+    }
+
+    private fieldData: FieldDataType = {} as FieldDataType
     private team: Team
-    private listeners : listenerType = {} as listenerType
+    private listeners: ListenerType = {} as ListenerType
+    private popoverTemplate = '<div class="popover" role="tooltip" style="min-width:200px"><div class="arrow"></div><h3 class="popover-title"></h3><div class="alert alert-s alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span><span class="sr-only">Error:</span><div class="popover-content popover-content-s"></div></div></div>'
 }
 
-interface listenerType {
-        homeGamesMin: Subscription,
-        homeGamesMax: Subscription,
-        homeGamesEnabled: Subscription,
-        awayGamesMin: Subscription,
-        awayGamesMax: Subscription,
-        awayGamesEnabled: Subscription
+interface ListenerType {
+    homeGamesMin: Subscription,
+    homeGamesMax: Subscription,
+    homeGamesEnabled: Subscription,
+    awayGamesMin: Subscription,
+    awayGamesMax: Subscription,
+    awayGamesEnabled: Subscription
+}
+interface PopoverFieldType {
+    element: JQuery,
+    state: State
+}
+interface FieldDataType {
+    homeGamesMin: PopoverFieldType,
+    homeGamesMax: PopoverFieldType,
+    awayGamesMin: PopoverFieldType,
+    awayGamesMax: PopoverFieldType
+}
+enum State {
+    Hidden,
+    Hidding,
+    Shown,
+    Showing
 }
