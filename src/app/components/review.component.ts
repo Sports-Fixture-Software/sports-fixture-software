6ff44da4f6403d82914c7ed851f47e42dfb4de0e
Craig S. Keogh <cskeogh@adam.com.au>
2016-10-03 15:30:04 +1030

    Add 'Edit Fixture' button and team buttons
    When the user clicks 'Edit Fixture' the page enters edit mode, and the
    teams become buttons & therefore clickable. Clicking the team button is
    yet to do.

--- src/app/components/review.component.ts
+++ src/app/components/review.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit, ChangeDetectorRef, } from '@angular/core'
-import { ActivatedRoute, Router, Params } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router'
+import { Validators } from '@angular/common'
 import { FixtureService } from '../services/fixture.service'
 import { Collection } from '../services/collection'
 import { Fixture } from '../models/fixture'
 import { Round } from '../models/round'
+import { Validator } from '../util/validator'
+import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms'
+import { POPOVER_DIRECTIVES, PopoverContent } from 'ng2-popover'
 
 @Component({
     moduleId: module.id.replace(/\\/g, '/'),
     providers: [FixtureService],
+    directives: [POPOVER_DIRECTIVES, REACTIVE_FORM_DIRECTIVES],
     templateUrl: 'review.template.html'
 })
 
@@ -18,9 +23,16 @@ export class ReviewComponent implements OnInit {
         private _route: ActivatedRoute) {
     }
 
+    matchupForm: FormGroup
     error: Error
 
     ngOnInit() {
+        this.matchupForm = new FormGroup({
+            round: new FormControl(),
+            homeTeam: new FormControl('', [<any>Validators.required]),
+            awayTeam: new FormControl('', [<any>Validators.required]),
+            config: new FormControl()
+        }, {}, Validator.differentTeamsSelected)
         this._router.routerState.parent(this._route)
             .params.forEach(params => {
                 let id = +params['id'];
@@ -38,6 +50,81 @@ export class ReviewComponent implements OnInit {
             })
     }
 
+    onEditFixture() {
+        this.editing = !this.editing
+        this.editFixtureLabel = this.editing ? ReviewComponent.VIEW_FIXTURE : ReviewComponent.EDIT_FIXTURE
+    }
+
+    /**
+     * Called after ngFor is complete.
+     *
+     * If, in the future, Angular2 provides native support for
+     * ngFor-on-complete, update this method.
+     */
+    onAfterFor() {
+        this.enableTooltipForOverflowedElements('.matchup-button')
+    }
+
+    /**
+     * Prepares the match-up form based on what the user selected.
+     *
+     * There is only one match-up form: the form changes based upon what round
+     * or match-up the user selects.
+     *
+     * `round` the selected round.
+     * `config` the selected match-up.
+     */
+    prepareForm(round: Round, config?: MatchConfig) {
+        let fc = this.matchupForm.controls['round'] as FormControl
+        fc.updateValue(round)
+        fc = this.matchupForm.controls['config'] as FormControl
+        fc.updateValue(config)
+        if (config && config.homeTeamPreLoaded) {
+            fc = this.matchupForm.controls['homeTeam'] as FormControl
+            for (let team of this.homeTeamsAll) {
+                if (team.id == config.homeTeamPreLoaded.id) {
+                    fc.updateValue(team)
+                    break
+                }
+            }
+        }
+        if (config && config.awayTeamPreLoaded) {
+            fc = this.matchupForm.controls['awayTeam'] as FormControl
+            for (let team of this.awayTeamsAll) {
+                if (team.id == config.awayTeamPreLoaded.id) {
+                    fc.updateValue(team)
+                    break
+                }
+            }
+        }
+        this.removeTeamsAsAlreadyReserved(round,
+            config ? config.homeTeamPreLoaded : null,
+            config ? config.awayTeamPreLoaded : null)
+    }
+
+    /**
+     * Enable a popup tooltip for overflowed elements. For example, if the
+     * text is too long for the button, display a tooltip showing the whole
+     * text.
+     *
+     * `selector` is jQuery selector string to select the elements.
+     */
+    private enableTooltipForOverflowedElements(selector: string) {
+        jQuery(selector).each((index, elem) => {
+            let jElem = jQuery(elem)
+            if (elem.scrollWidth > jElem.innerWidth()) {
+                jElem.tooltip({
+                    delay: { 'show': 1000, 'hide': 100 },
+                    trigger: 'hover'
+                })
+            }
+        })
+    }
+
+    private static EDIT_FIXTURE: string = 'Edit Fixture'
+    private static VIEW_FIXTURE: string = 'View Fixture'
+    private editFixtureLabel: string = ReviewComponent.EDIT_FIXTURE
+    private editing: boolean = false
     private rounds: Round[] = []
     private fixture: Fixture
 }
