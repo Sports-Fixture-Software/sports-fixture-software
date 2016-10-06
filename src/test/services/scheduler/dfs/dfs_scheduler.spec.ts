a43912d2e79dbd4c2944b696cb5a2db17963b0e4
Craig S. Keogh <cskeogh@adam.com.au>
2016-10-06 22:14:37 +1030

    Convert the DFS demo driver to unit tests
    Runs 3 unit tests. Checking the return values programatically (rather than
    console.log) is still to do.

--- src/test/services/scheduler/dfs/dfs_scheduler.spec.ts
+++ src/test/services/scheduler/dfs/dfs_scheduler.spec.ts
@@ -1,133 +1,84 @@
-import { plotFixtureRotation } from './dfsFixtureDemo';
-import { Match, Constraint, FixtureInterface, Team } from './FixtureConstraints';
-
-
-/**
- * The following is test driver code for the dfsFixtureDemo function
- */
-
-// TEAM IMPLEMENTATION CLASSES W. DUMMY CONSTRAINTS
+import { plotFixtureRotation } from '../../../../app/services/scheduler/dfs/plot_fixture_rotation';
+import { Match, Constraint, FixtureInterface, Team } from '../../../../app/services/scheduler/dfs/fixture_constraints';
 
 // No special constraints
 class TestTeamNoConstraints implements Team {
-    
-    constructor(){}
 
-    constraintsSatisfied( fixture: FixtureInterface, proposedMatch: Match, home: boolean ): Constraint {
+    constructor() { }
+
+    constraintsSatisfied(fixture: FixtureInterface, proposedMatch: Match, home: boolean): Constraint {
         return Constraint.SATISFIED;
     }
 }
 
-// Maximum home games per rotation
-/*class TestTeamMaxHome implements Team {
-    
-    constructor(){}
+describe('services DFS scheduler', () => {
+    it('6 teams, no constraints', () => {
+        let reservedMatches: Match[] = [];
+        let testTeams = [
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints()
+        ];
+        let testFixture = plotFixtureRotation(testTeams, reservedMatches, true);
+        expect(testFixture).toBeDefined()
+        console.log("Success. Printing fixture:");
+        for (var i: number = 0; i < testFixture.length; i++) {
+            console.log("Round " + testFixture[i].roundNum + ": H" + testFixture[i].homeTeam + " vs. A" + testFixture[i].awayTeam);
+        }
+    })
 
-    // HASHMAP TO STORE TEAM LEVEL CONSTRAINTS
-    // HASHMAP TO STORE FIXTURE LEVEL CONSTRAINTS
-    // HASHMAP TO STORE LEAGUE LEVEL CONSTRAINTS
+    it('6 teams, two reserved games', () => {
+        let reservedMatches: Match[] = [
+            new Match(0, 0, 1),
+            new Match(1, 0, 2)
+        ]
+        let testTeams = [
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints()
+        ];
+        let testFixture = plotFixtureRotation(testTeams, reservedMatches, true);
+        expect(testFixture).toBeDefined()
+        console.log("Success. Printing fixture:");
+        for (var i: number = 0; i < testFixture.length; i++) {
+            console.log("Round " + testFixture[i].roundNum + ": H" + testFixture[i].homeTeam + " vs. A" + testFixture[i].awayTeam);
+        }
+    })
 
-    constraintsSatisfied( fixture: FixtureInterface, proposedMatch: Match, home: boolean ): Constraint {
-        // Run through hashmaps to check that constraints are satisfied
+    it('6 teams, full reserved games', () => {
+        let reservedMatches: Match[] = [ // This should break as it is not a legal setup
+            new Match(0, 0, 1), new Match(0, 2, 5), new Match(0, 3, 4),
+            new Match(1, 0, 2), new Match(1, 1, 3), new Match(1, 5, 4),
+            new Match(2, 2, 4), new Match(2, 0, 3), new Match(2, 1, 5),
+            new Match(3, 5, 0), new Match(3, 3, 2), new Match(3, 4, 1),
+            new Match(4, 0, 4), new Match(4, 5, 3), new Match(4, 1, 2)
+        ]
+        let testTeams = [
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints()
+        ];
+        let testFixture = plotFixtureRotation(testTeams, reservedMatches, true);
+        expect(testFixture).toBeDefined()
+        console.log("Success. Printing fixture:");
+        for (var i: number = 0; i < testFixture.length; i++) {
+            console.log("Round " + testFixture[i].roundNum + ": H" + testFixture[i].homeTeam + " vs. A" + testFixture[i].awayTeam);
+        }
+    })
 
-        return Constraint.SATISFIED;
-    } 
-}*/
-
-// Minimum away games per rotation
-/*class TestTeamMinAway implements Team {
-    
-    constructor(){}
-
-    // HASHMAP TO STORE TEAM LEVEL CONSTRAINTS
-    // HASHMAP TO STORE FIXTURE LEVEL CONSTRAINTS
-    // HASHMAP TO STORE LEAGUE LEVEL CONSTRAINTS
-
-    constraintsSatisfied( fixture: FixtureInterface, proposedMatch: Match, home: boolean ): Constraint {
-        // Run through hashmaps to check that constraints are satisfied
-
-        return Constraint.SATISFIED;
-    } 
-}*/
-
-// Maximum consecutive home and away games per rotation
-/*class TestTeamMaxConsecAwayAndHome implements Team {
-    
-    constructor(){}
-
-    // HASHMAP TO STORE TEAM LEVEL CONSTRAINTS
-    // HASHMAP TO STORE FIXTURE LEVEL CONSTRAINTS
-    // HASHMAP TO STORE LEAGUE LEVEL CONSTRAINTS
-
-    constraintsSatisfied( fixture: FixtureInterface, proposedMatch: Match, home: boolean ): Constraint {
-        // Run through hashmaps to check that constraints are satisfied
-
-        return Constraint.SATISFIED;
-    } 
-}*/
-
-// DRIVER TO TEST THE plotFixtureRotation FUNCTION
-
-
-
-console.log("DFS TESTING...");
-
-var testFixture: Match[] = [];
-var testTeams: Team[] = [];
-var reservedMatches: Match[] = [];
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-console.log("===== TEST 1: 6 teams, no constraints =====");
-testTeams = [
-    new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
-    new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints()
-];
-testFixture = plotFixtureRotation( testTeams, reservedMatches, true );
-console.log("Success. Printing fixture:");
-for( var i: number = 0; i < testFixture.length; i++ ){
-    console.log("Round " + testFixture[i].roundNum + ": H" + testFixture[i].homeTeam + " vs. A" + testFixture[i].awayTeam);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-console.log("===== TEST 2: 6 teams, two reserved games =====");
-testTeams = [
-    new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
-    new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints()
-];
-reservedMatches = [
-    new Match(0,0,1),
-    new Match(1,0,2)
-];
-testFixture = plotFixtureRotation( testTeams, reservedMatches, true );
-console.log("Success. Printing fixture:");
-for( var i: number = 0; i < testFixture.length; i++ ){
-    console.log("Round " + testFixture[i].roundNum + ": H" + testFixture[i].homeTeam + " vs. A" + testFixture[i].awayTeam);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-console.log("===== TEST 3: 6 teams, full reserved games =====");
-testTeams = [
-    new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
-    new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints()
-];
-reservedMatches = [ // This should break as it is not a legal setup
-    new Match(0,0,1), new Match(0,2,5), new Match(0,3,4),
-    new Match(1,0,2), new Match(1,1,3), new Match(1,5,4),
-    new Match(2,2,4), new Match(2,0,3), new Match(2,1,5),
-    new Match(3,5,0), new Match(3,3,2), new Match(3,4,1), 
-    new Match(4,0,4), new Match(4,5,3), new Match(4,1,2)  
-];
-testFixture = plotFixtureRotation( testTeams, reservedMatches, true );
-console.log("Success. Printing fixture:");
-for( var i: number = 0; i < testFixture.length; i++ ){
-    console.log("Round " + testFixture[i].roundNum + ": H" + testFixture[i].homeTeam + " vs. A" + testFixture[i].awayTeam);
-}
+    // 10 teams - this currently takes a long time (too long)
+    /*
+    it('10 teams, no constraints', () => {
+        let reservedMatches: Match[] = [];
+        let testTeams = [
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
+            new TestTeamNoConstraints(), new TestTeamNoConstraints(), new TestTeamNoConstraints(),
+            new TestTeamNoConstraints()
+        ];
+        let testFixture = plotFixtureRotation(testTeams, reservedMatches, true);
+        expect(testFixture).toBeDefined()
+        console.log("Success. Printing fixture:");
+        for (var i: number = 0; i < testFixture.length; i++) {
+            console.log("Round " + testFixture[i].roundNum + ": H" + testFixture[i].homeTeam + " vs. A" + testFixture[i].awayTeam);
+        }
+    })
+    */
+})
