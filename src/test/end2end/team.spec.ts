124a6b2a16d06009b58248f0066965fb074c8299
Craig S. Keogh <cskeogh@adam.com.au>
2016-10-10 22:38:30 +1030

    Add end-to-end tests for team functions
    Tests for:
    - delete team
    - edit team
    - revert team

--- src/test/end2end/team.spec.ts
+++ src/test/end2end/team.spec.ts
@@ -6,7 +6,7 @@ import * as Promise from 'bluebird'
 
 describe('team', function () {
     let app: any
-    
+
     beforeEach((done) => {
         app = TestApp.startApp(done)
     }, 7000 * computerSpeed)
@@ -39,4 +39,112 @@ describe('team', function () {
             done()
         })
     }, 7000 * computerSpeed)
+
+    it('delete team', (done) => {
+        let client = app.client as webdriverio.Client<void>
+        let leagueName = 'alpha'
+        let teamName = 'apple'
+        createLeague(client, leagueName).then(() => {
+            return client.click(`[aria-label="${leagueName}"]`)
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Edit League Form"]')
+        }).then(() => {
+            return client.click('[aria-label="Teams"]')
+        }).then(() => {
+            return createTeam(client, teamName)
+        }).then(() => {
+            return client.waitForVisible(`[aria-label="${teamName}"]`)
+        }).then(() => {
+            return client.click(`[aria-label="${teamName}"]`)
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Delete Team"]')
+        }).then(() => {
+            return client.click('[aria-label="Delete Team"]')
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Confirm Delete Team"]')
+        }).then(() => {
+            return client.click('[aria-label="Confirm Delete Team"]')
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Create Team"]')
+        }).then(() => {
+            return client.isExisting('team-list-item')
+        }).then((exists) => {
+            expect(exists).toBe(false)
+            done()
+        })
+    }, 8000 * computerSpeed)
+
+    it('edit team', (done) => {
+        let client = app.client as webdriverio.Client<void>
+        let leagueName = 'alpha'
+        let teamName = 'apple'
+        let editName = 'banana'
+        createLeague(client, leagueName).then(() => {
+            return client.click(`[aria-label="${leagueName}"]`)
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Edit League Form"]')
+        }).then(() => {
+            return client.click('[aria-label="Teams"]')
+        }).then(() => {
+            return createTeam(client, teamName)
+        }).then(() => {
+            return client.waitForVisible(`[aria-label="${teamName}"]`)
+        }).then(() => {
+            return client.click(`[aria-label="${teamName}"]`)
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Edit Team"]')
+        }).then(() => {
+            return client.click('[aria-label="Edit Team"]')
+        }).then(() => {
+            return client.waitForVisible('input[aria-labelledby="teamName"]')
+        }).then(() => {
+            return client.setValue('input[aria-labelledby="teamName"]', editName)
+        }).then(() => {
+            return client.submitForm('input[aria-labelledby="teamName"]')
+        }).then(() => {
+            return client.waitForVisible('div[aria-labelledby="teamName"]')
+        }).then(() => {
+            return client.getText('div[aria-labelledby="teamName"]')
+        }).then((text) => {
+            expect(text).toBe(editName)
+            done()
+        })
+    }, 8000 * computerSpeed)
+
+    it('revert team', (done) => {
+        let client = app.client as webdriverio.Client<void>
+        let leagueName = 'alpha'
+        let teamName = 'apple'
+        let editName = 'banana'
+        createLeague(client, leagueName).then(() => {
+            return client.click(`[aria-label="${leagueName}"]`)
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Edit League Form"]')
+        }).then(() => {
+            return client.click('[aria-label="Teams"]')
+        }).then(() => {
+            return createTeam(client, teamName)
+        }).then(() => {
+            return client.waitForVisible(`[aria-label="${teamName}"]`)
+        }).then(() => {
+            return client.click(`[aria-label="${teamName}"]`)
+        }).then(() => {
+            return client.waitForVisible('[aria-label="Edit Team"]')
+        }).then(() => {
+            return client.click('[aria-label="Edit Team"]')
+        }).then(() => {
+            return client.waitForVisible('input[aria-labelledby="teamName"]')
+        }).then(() => {
+            return client.setValue('input[aria-labelledby="teamName"]', editName)
+        }).then(() => {
+            return client.click('[aria-label="Revert"]')
+        }).then(() => {
+            return client.waitForVisible('div[aria-labelledby="teamName"]')
+        }).then(() => {
+            return client.getText('div[aria-labelledby="teamName"]')
+        }).then((text) => {
+            expect(text).toBe(teamName)
+            done()
+        })
+    }, 8000 * computerSpeed)
 })
