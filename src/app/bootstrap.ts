f5e11ca3b912a288e6853c9328d2efdeaefb48a2
Michael Humphris <a1631155@student.adelaide.edu.au>
2016-10-16 18:55:05 +1030

    created application module

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,22 +1,18 @@
 ///<reference path="../../typings/index.d.ts" />
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { DatabaseService } from './services/database.service'
 import { ReflectiveInjector, Injector } from '@angular/core'
 export let databaseInjector: Injector = ReflectiveInjector.resolveAndCreate([DatabaseService])
 
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-import { APP_ROUTER_PROVIDERS } from './app.routes';
-import { LocationStrategy,
-         HashLocationStrategy } from '@angular/common';
-import { disableDeprecatedForms, provideForms } from '@angular/forms';
+import { AppModule } from './app.module';
 
 let dbs: DatabaseService = databaseInjector.get(DatabaseService)
 dbs.init().then(() => {
-    bootstrap(AppComponent, [
-        APP_ROUTER_PROVIDERS,
-        disableDeprecatedForms(),
-        provideForms(),
-        { provide: LocationStrategy, useClass: HashLocationStrategy } // Use hash location strategy to play nice with electron's filesystem URLs
-    ])
+    const platform = platformBrowserDynamic();
+    platform.bootstrapModule(AppModule);
 });
+
+
+
+
