e25634bdf41fe4df04efeac54ce74897cd86eb22
Michael Humphris <a1631155@student.adelaide.edu.au>
2016-10-23 20:54:10 +1030

    added utility to create a fixture CSV sorted by teams

--- src/app/util/export_to.ts
+++ src/app/util/export_to.ts
@@ -21,6 +21,42 @@ export class ExportTo {
     }
 
     /**
+     * Convert the generated fixture (represented by `rounds`) to a comma
+     * separated values (CSV) format such that matches are grouped by teams.
+     * The output is written to `stream`.  The `rounds` must be eager loaded.
+     */
+    static teamCSV(stream: fs.WriteStream, rounds: Round[]) {
+        var teamMap : {[key: string]: string[]} = {}
+        var roundInfo: string[] = []
+        for (let round of rounds) {
+            roundInfo.push(round.number + ',' + this.CSVify(round.startDate.format('YYYY-MM-DD')))
+
+            for (let match of round.matchesPreLoaded) {
+                if (match.homeTeamPreLoaded && match.awayTeamPreLoaded) {
+                    if (!(match.homeTeamName in teamMap)) {
+                        teamMap[match.homeTeamName] = []
+                    }
+                    if (!(match.awayTeamName in teamMap)) {
+                        teamMap[match.awayTeamName] = []
+                    }
+
+                    teamMap[match.homeTeamName].push('Home,' + this.CSVify(match.awayTeamName))
+                    teamMap[match.awayTeamName].push('Away,' + this.CSVify(match.homeTeamName))
+                }
+            }
+        }
+
+        for (var team in teamMap) {
+            stream.write(this.CSVify(team) + EOL)
+            let matches = teamMap[team]
+            matches.forEach((current, idx, arr)=>{
+                stream.write(roundInfo[idx] + ','  + current + EOL)
+            })
+            stream.write(',' + EOL)
+        }
+    }
+
+    /**
      * Convert a string into a CSV string. If the string contains " or , or \n
      * the string is wrapped in " ". If the string contains ", the " is
      * replaced with "".
